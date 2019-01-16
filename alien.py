2753ef5a8468a929ebf40d9eaa591b6a2eb63fa5
pingguoyuanabc <45932989+pingguoyuanabc@users.noreply.github.com>
2019-01-16 16:15:25 +0800

    Add files via upload

--- alien.py
+++ alien.py
@@ -1,40 +1,17 @@
-import pygame
-from pygame.sprite import Sprite
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original position: " + str(alien_0['x_position']))
 
-class Alien(Sprite):
-    """A class to represent a single alien in the fleet."""
+# Move the alien to the right.
+# Figure out how far to move the alien based on its speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
 
-    def __init__(self, ai_settings, screen):
-        """Initialize the alien, and set its starting position."""
-        super(Alien, self).__init__()
-        self.screen = screen
-        self.ai_settings = ai_settings
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
 
-        # Load the alien image, and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
-        self.rect = self.image.get_rect()
-
-        # Start each new alien near the top left of the screen.
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-
-        # Store the alien's exact position.
-        self.x = float(self.rect.x)
-        
-    def check_edges(self):
-        """Return True if alien is at edge of screen."""
-        screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right:
-            return True
-        elif self.rect.left <= 0:
-            return True
-        
-    def update(self):
-        """Move the alien right or left."""
-        self.x += (self.ai_settings.alien_speed_factor *
-                        self.ai_settings.fleet_direction)
-        self.rect.x = self.x
-
-    def blitme(self):
-        """Draw the alien at its current location."""
-        self.screen.blit(self.image, self.rect)
+print("New position: " + str(alien_0['x_position']))
